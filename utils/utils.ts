02e80c8bd556dc6a48ef0bae37c7b3a17f82fac5
glitcher93 <nigeljdsouza@gmail.com>
2022-04-15 21:10:36 -0400

    added type assertion and changed commonjs exports to es6 module exports

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,16 +1,13 @@
 import fs from 'fs';
 import { Video } from './interfaces';
 
-let videoRead = () => {
+export const videoRead = () => {
     const videoData = fs.readFileSync('./data/video-details.json');
     const videoParse = JSON.parse(videoData.toString())
     return videoParse;
 }
 
-let videoWrite = (data) => {
+export const videoWrite = (data: Video[]) => {
     const stringifiedData = JSON.stringify(data)
     fs.writeFileSync('./data/video-details.json', stringifiedData)
 }
-
-exports.videoRead = videoRead;
-exports.videoWrite = videoWrite;
