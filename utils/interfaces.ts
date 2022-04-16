6c53b40792d2837c69be1265d833873972a3083f
glitcher93 <nigeljdsouza@gmail.com>
2022-04-15 21:01:54 -0400

    set up interfaces needed for server

---
+++ utils/interfaces.ts
@@ -0,0 +1,21 @@
+export interface Video {
+    id: string
+    name: string
+    image: string
+    channel: string
+    description: string
+    views: string
+    likes: number
+    duration: string
+    video: string
+    timestamp: number
+    comments: Comment[]
+}
+
+export interface Comment {
+    id: string
+    name: string
+    comment: string
+    likes: number
+    timestamp: number
+}
