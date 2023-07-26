a3dfb66832649ddf20c7b9ea85e285d5cf1bf5ef
glitcher93 <nigeljdsouza@gmail.com>
2023-07-25 22:40:46 -0400

    removed jsonServer integration

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 import videoRoutes from './routes/videos';
-import jsonServer from 'json-server';
 
 dotenv.config()
 
@@ -11,7 +9,6 @@ const app: Express = express();
 const { PORT } = process.env || 8082;
 
 app.use(cors());
-app.use(jsonServer.router('./data/video-details.json'));
 app.use(express.json());
 app.use(express.static('public'))
 
