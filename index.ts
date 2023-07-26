47408a290954d48e25c77263df87759a4e491b31
glitcher93 <nigeljdsouza@gmail.com>
2023-07-25 21:51:02 -0400

    integrated jsonServer

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import videoRoutes from './routes/videos';
+import jsonServer from 'json-server';
 
 dotenv.config()
 
@@ -9,6 +11,7 @@ const app: Express = express();
 const { PORT } = process.env || 8082;
 
 app.use(cors());
+app.use(jsonServer.router(path.join(__dirname, 'data/video-details.json')));
 app.use(express.json());
 app.use(express.static('public'))
 
